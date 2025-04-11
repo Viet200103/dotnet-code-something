674d2120da24730337a0807f910352db2c8082d7
VietTL <viettl200103@gmail.com>
2025-04-11 20:46:54 +0700

    Add try to learn example

---
+++ CSharpTryToLearn/Examples/StreamExample.cs
@@ -0,0 +1,53 @@
+﻿namespace CSharpTryToLearn.Examples;
+
+public class StreamExample
+{
+
+    [Program.RunnableEntry]
+    public static void Run()
+    {
+        // using (Stream s = new FileStream("test.txt", FileMode.Create))
+        // {
+        //     Console.WriteLine("Can read: " + s.CanRead);
+        //     Console.WriteLine("Can write: " + s.CanWrite);
+        //     Console.WriteLine("Can seek: " + s.CanSeek);
+        //     
+        //     s.WriteByte(101);
+        //     s.WriteByte(102);
+        //     byte[] block = { 1, 2, 3, 4, 5 };
+        //     s.Write(block, 0, block.Length);
+        //     
+        //     Console.WriteLine(s.Length);
+        //     Console.WriteLine(s.Position);
+        //     s.Position = 0;
+        //     
+        //     Console.WriteLine(s.ReadByte());
+        //     Console.WriteLine(s.ReadByte());
+        //     
+        //     Console.WriteLine(s.Read(block, 0, block.Length));
+        //     Console.WriteLine(s.Read(block, 0, block.Length));
+        // }
+
+        DriveInfo c = new DriveInfo("C");
+        
+        long totalSize = c.TotalSize;
+        long freeBytes = c.TotalFreeSpace;
+        long freeToMe = c.AvailableFreeSpace;
+
+        Console.WriteLine($"Total size: {totalSize}");
+        Console.WriteLine($"Free bytes: {freeBytes}");
+        Console.WriteLine($"Free To Me: {freeToMe}");
+        
+        foreach (DriveInfo drive in DriveInfo.GetDrives())
+        {
+            Console.WriteLine(drive.Name);
+            Console.WriteLine(drive.DriveType);
+            Console.WriteLine(drive.RootDirectory);
+            if (drive.IsReady)
+            {
+                Console.WriteLine(drive.VolumeLabel);
+                Console.WriteLine(drive.DriveFormat);
+            }
+        }
+    }
+}
---
+++ CSharpTryToLearn/Examples/TimeExample.cs
@@ -0,0 +1,108 @@
+﻿
+namespace CSharpTryToLearn.Examples;
+
+public class TimeExample
+{
+    public static void Run()
+    {
+        // Console.WriteLine(new TimeSpan(2, 30, 0));
+        // Console.WriteLine(TimeSpan.FromHours(2.5));
+        // Console.WriteLine(TimeSpan.FromDays(-2.5));
+
+        // Console.WriteLine("------------------------------------------------");
+
+        // Console.WriteLine(TimeSpan.FromHours(2) + TimeSpan.FromMinutes(20));
+        // Console.WriteLine(TimeSpan.FromDays(10) - TimeSpan.FromSeconds(1));
+
+        // Console.WriteLine("------------------------------------------------");
+
+        // TimeSpan nearlyTenDays = TimeSpan.FromDays(10) - TimeSpan.FromSeconds(1);
+        //
+        // Console.WriteLine(nearlyTenDays.TotalDays);
+        // Console.WriteLine(nearlyTenDays.TotalHours);
+        // Console.WriteLine(nearlyTenDays.TotalMinutes);
+        // Console.WriteLine(nearlyTenDays.TotalMilliseconds);
+
+        // Console.WriteLine("------------------------------------------------");
+        // Console.WriteLine("--------------------DateTime--------------------");
+        //
+        // DateTime dt = new DateTime(2000, 2, 3, 10, 20, 30);
+        //
+        // Console.WriteLine(dt.Year);
+        // Console.WriteLine(dt.Month);
+        // Console.WriteLine(dt.Day);
+        // Console.WriteLine(dt.DayOfWeek);
+        // Console.WriteLine(dt.DayOfYear);
+        // Console.WriteLine(dt.Hour);
+        // Console.WriteLine(dt.Minute);
+        // Console.WriteLine(dt.Second);
+        // Console.WriteLine(dt.Millisecond);
+        // Console.WriteLine(dt.TimeOfDay); // TimeSpan
+        //
+
+        // Console.WriteLine("-------------------------------------------------------------");
+        // Console.WriteLine("--------------------DateTime & Time Zones--------------------");
+
+        // DateTime dt1 = new DateTime(2000, 1, 1, 10, 20, 30, DateTimeKind.Local);
+        // DateTime dt2 = new DateTime(2000, 1, 1, 10, 20, 30, DateTimeKind.Utc);
+        // Console.WriteLine(dt1 == dt2);
+        // DateTime local = DateTime.Now;
+        // DateTime utc = local.ToUniversalTime();
+        // Console.WriteLine(local == utc);
+
+        // DateTime d = new DateTime(2015, 12, 12);
+        // DateTime utc = DateTime.SpecifyKind(d, DateTimeKind.Utc);
+        // Console.WriteLine(utc);
+
+        // TimeZoneInfo zone = TimeZoneInfo.Local;
+        // Console.WriteLine(zone.StandardName);
+        // Console.WriteLine(zone.DaylightName);
+
+        // DateTime dt1 = new DateTime(2025, 4, 9);
+        // DateTime dt2 = new DateTime(2025, 3, 1);
+        // Console.WriteLine(zone.IsDaylightSavingTime(dt1));
+        // Console.WriteLine(zone.IsDaylightSavingTime(dt2));
+        // Console.WriteLine(zone.GetUtcOffset(dt1));
+        // Console.WriteLine(zone.GetUtcOffset(dt2));
+
+        // TimeZoneInfo we = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+        // Console.WriteLine(we.Id);
+        // Console.WriteLine(we.BaseUtcOffset);
+        // Console.WriteLine(we.SupportsDaylightSavingTime);
+
+        // Console.WriteLine("-------------------------------------------------------------");
+        // Console.WriteLine("-----------------------------Process-------------------------");
+
+        // ProcessStartInfo psi = new ProcessStartInfo
+        // {
+        //     FileName = "cmd.exe",
+        //     Arguments = "/c ipconfig /all",
+        //     RedirectStandardOutput = true,
+        //     UseShellExecute = false
+        // };
+        //
+        // Process? p = Process.Start(psi);
+        // if (p != null)
+        // {
+        //     string result = p.StandardOutput.ReadToEnd();
+        //     Console.WriteLine(result);
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Process was not started.");
+        // }
+        
+        // Console.WriteLine("-------------------------------------------------------------");
+        // Console.WriteLine("---------------------------Collection------------------------");
+        //
+        // string s = "Hello";
+        //
+        // IEnumerator rator = s.GetEnumerator();
+        // while (rator.MoveNext())
+        // {
+        //     char c = (char) rator.Current;
+        //     Console.Write(c + ".");
+        // }
+        //
+    }
+}
